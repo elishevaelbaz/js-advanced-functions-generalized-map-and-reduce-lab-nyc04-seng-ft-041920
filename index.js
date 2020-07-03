1ca90cd79b59cd77b73f0a761f39f502765cb2fc
github username <github email address>
2020-07-02 21:55:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+function map(arr, cb){
+  let newArr = []
+  for (let i = 0; i < arr.length; i++){
+    newArr.push(cb(arr[i]))
+  }
+  return newArr
+}
+
+
+function reduce(arr, cb, starting){
+  let sum = (!!starting) ? starting : arr[0]
+
+  for (let i = (!!starting) ? 0 : 1; i < arr.length; i++){
+    sum = cb(arr[i], sum)
+  }
+  return sum
+}
